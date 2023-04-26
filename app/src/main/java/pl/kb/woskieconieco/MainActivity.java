fbc061b7138d5a21f64a66b342fd46dcc3aed3b8
Kacper Babka <kacper.babka@gmail.com>
2023-04-26 10:32:19 +0200

    Second commit

--- app/src/main/java/pl/kb/woskieconieco/MainActivity.java
+++ app/src/main/java/pl/kb/woskieconieco/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //This will be second commit
         }
 }
