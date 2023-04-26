993a0099e4f1a3fa6cbb2226f9bcc8f9dfa2ca98
Kacper Babka <kacper.babka@gmail.com>
2023-04-26 10:27:13 +0200

    First Commit

---
+++ app/src/main/java/pl/kb/woskieconieco/OrderActivity.java
@@ -0,0 +1,17 @@
+package pl.kb.woskieconieco;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
